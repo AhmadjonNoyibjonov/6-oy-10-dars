9b464af696412a41a84a3b8c62fa8f930118a045
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-08-05 12:16:01 +0500

    changed

--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,41 +1,41 @@
-import React, { useContext } from 'react';
-import styles from './index.module.css';
-import { ThemeContext } from '../../App';
+import React, { useContext } from "react";
+import styles from "./index.module.css";
+import { ThemeContext } from "../../App";
 
 function Footer() {
-  const {theme} = useContext(ThemeContext);
-    
+  const { theme } = useContext(ThemeContext);
+
   return (
     <footer className={`${styles.footer} ${styles[theme]}`}>
       <div className={styles.footerItems}>
         <div className={styles.footerItem}>
-            <img src="./src/assets/dairy.png" alt="" />
+          <img src="src/assets/dairy.png" alt="" />
           <h3>Dairy Products</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/fruits.png" alt="" />
+          <img src="/src/assets/fruits.png" alt="" />
           <h3>Vegetables & Fruits</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/condiments.png" alt="" />
+          <img src="/src/assets/condiments.png" alt="" />
           <h3>Spices & Seasonings</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/babyfood.png" alt="" />
+          <img src="/src/assets/babyfood.png" alt="" />
           <h3>Honey</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className={styles.footerItem}>
-            <img src="./src/assets/grain.png" alt="" />
+          <img src="/src/assets/grain.png" alt="" />
           <h3>Flour</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -14,7 +14,8 @@ function Hero() {
     en: {
       welcome: "Welcome to our site",
       groceries: "groceries",
-      description: "Get fresh groceries online without stepping out to make delicious food with the freshest ingredients.",
+      description:
+        "Get fresh groceries online without stepping out to make delicious food with the freshest ingredients.",
       searchPlaceholder: "Search here",
       freshVegetables: "Fresh Vegetables",
       guarantee: "100% Guarantee",
@@ -26,7 +27,8 @@ function Hero() {
     uz: {
       welcome: "Bizning saytga xush kelibsiz",
       groceries: "sabzavotlar",
-      description: "Eng yangi mahsulotlarni onlayn tarzda oling va mazali taomlar tayyorlang.",
+      description:
+        "Eng yangi mahsulotlarni onlayn tarzda oling va mazali taomlar tayyorlang.",
       searchPlaceholder: "Qidirish",
       freshVegetables: "Yangi sabzavotlar",
       guarantee: "100% Kafolat",
@@ -38,7 +40,8 @@ function Hero() {
     ru: {
       welcome: "Добро пожаловать на наш сайт",
       groceries: "продукты",
-      description: "Получайте свежие продукты онлайн, не выходя из дома, чтобы приготовить вкусную еду из свежайших ингредиентов.",
+      description:
+        "Получайте свежие продукты онлайн, не выходя из дома, чтобы приготовить вкусную еду из свежайших ингредиентов.",
       searchPlaceholder: "Поиск",
       freshVegetables: "Свежие овощи",
       guarantee: "100% Гарантия",
@@ -46,90 +49,90 @@ function Hero() {
       fastDelivery: "Быстрая доставка",
       freshSpinach: "Свежий шпинат",
       freshCarrot: "Свежая морковь",
-    }
+    },
   };
 
   const t = texts[language];
 
   return (
     <div
-    className={`${styles.hero} ${
-      theme === "dark" ? styles.darkMode : styles.lightMode
-    }`}
-  >
-    <div className={styles.heroText}>
-      <span className={styles.btnSpan}>
-        <button
-          className={`${styles.mode} ${
-            theme === "light" ? styles.active : ""
-          }`}
-          onClick={() => handleClick("light")}
-        >
-          <img
-            width={45}
-            height={35}
-            src="https://cdn0.iconfinder.com/data/icons/small-nature/50/3_Sep-11-512.png"
-            alt="sun"
-          />
-        </button>
-        <button
-          className={`${styles.mode} ${
-            theme === "dark" ? styles.active : ""
-          }`}
-          onClick={() => handleClick("dark")}
-        >
-          <i className="fa-solid fa-moon"></i>
-        </button>
-      </span>
-      <h1>
-        {t.welcome} <span className={styles.highlight}>{t.groceries}</span>
-      </h1>
-      <p>{t.description}</p>
-      <div className={styles.searchBar}>
-        <input type="text" placeholder={t.searchPlaceholder} />
-        <button type="button">
-          <i className="fa-solid fa-magnifying-glass"></i>
-        </button>
-      </div>
-      <div className={styles.searchinfo}>
-        <span className={styles.span}>
-          <span>
-            <i className="fa-regular fa-circle-check"></i> {t.freshVegetables}
-          </span>
-          <span>
-            <i className="fa-regular fa-circle-check"></i> {t.guarantee}
-          </span>
+      className={`${styles.hero} ${
+        theme === "dark" ? styles.darkMode : styles.lightMode
+      }`}
+    >
+      <div className={styles.heroText}>
+        <span className={styles.btnSpan}>
+          <button
+            className={`${styles.mode} ${
+              theme === "light" ? styles.active : ""
+            }`}
+            onClick={() => handleClick("light")}
+          >
+            <img
+              width={45}
+              height={35}
+              src="https://cdn0.iconfinder.com/data/icons/small-nature/50/3_Sep-11-512.png"
+              alt="sun"
+            />
+          </button>
+          <button
+            className={`${styles.mode} ${
+              theme === "dark" ? styles.active : ""
+            }`}
+            onClick={() => handleClick("dark")}
+          >
+            <i className="fa-solid fa-moon"></i>
+          </button>
         </span>
-        <span className={styles.span}>
-          <span>
-            <i className="fa-regular fa-circle-check"></i> {t.cashOnDelivery}
+        <h1>
+          {t.welcome} <span className={styles.highlight}>{t.groceries}</span>
+        </h1>
+        <p>{t.description}</p>
+        <div className={styles.searchBar}>
+          <input type="text" placeholder={t.searchPlaceholder} />
+          <button type="button">
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+        <div className={styles.searchinfo}>
+          <span className={styles.span}>
+            <span>
+              <i className="fa-regular fa-circle-check"></i> {t.freshVegetables}
+            </span>
+            <span>
+              <i className="fa-regular fa-circle-check"></i> {t.guarantee}
+            </span>
           </span>
-          <span>
-            <i className="fa-regular fa-circle-check"></i> {t.fastDelivery}
+          <span className={styles.span}>
+            <span>
+              <i className="fa-regular fa-circle-check"></i> {t.cashOnDelivery}
+            </span>
+            <span>
+              <i className="fa-regular fa-circle-check"></i> {t.fastDelivery}
+            </span>
           </span>
-        </span>
+        </div>
+      </div>
+      <div>
+        <img
+          className={styles.heroImage}
+          src="src/assets/hero.png"
+          alt="Groceries delivery"
+        />
+      </div>
+      <div className={styles.heroImg}>
+        <div className={styles.wrapper}>
+          <img src="src/assets/heroImage.png" alt="" />
+          <p>{t.freshSpinach}</p>
+          <p>$12.00</p>
+        </div>
+        <div className={styles.wrapper}>
+          <img className={styles.image} src="src/assets/image.png" alt="" />
+          <p>{t.freshCarrot}</p>
+          <p>$9.00</p>
+        </div>
       </div>
     </div>
-    <div>
-      <img
-        className={styles.heroImage}
-        src="./src/assets/hero.png"
-        alt="Groceries delivery"
-      />
-    </div>
-    <div className={styles.heroImg}>
-      <div className={styles.wrapper}>
-        <img src="./src/assets/heroImage.png" alt="" />
-        <p>{t.freshSpinach}</p>
-        <p>$12.00</p>
-      </div>
-      <div className={styles.wrapper}>
-        <img className={styles.image} src="./src/assets/image.png" alt="" />
-        <p>{t.freshCarrot}</p>
-        <p>$9.00</p>
-      </div>
-    </div>
-  </div>
   );
 }
 
